fae3dd1114c10f6248d500f45f97665dfaaeb6b6
A-N-I-K-A <bushra.anika2018@gmail.com>
2025-08-19 15:08:20 +0600

    Control, Conditional Statement and Array

--- demo.py
+++ demo.py
@@ -1,2 +1,73 @@
-print("hello")
-print(3+3)
+# //swap
+from sys import argv
+
+# x=5
+# y=6
+# x,y=y,x
+# print(x,y)
+# x=x^y
+# y=x^y
+# x=x^y
+# print(x,y)
+#
+#
+# x=int(input("Input a number:"))
+# y=int(input("Input a number:"))
+# z=x+y
+# print(z)
+#
+# #we only want to input one character
+# ch = input("Input a character:")[0]
+# print(ch)
+#
+# #using eval
+# result=eval(input("Input an expression:"))
+# print(result)
+
+#we can pass values with the python file_name.py val1 val2
+#like this, and run it from the terminal
+# x=(argv[1])
+# y=(argv[2])
+# print(x+y)
+
+
+# i=1
+# while i<=5:
+#     print(i)
+#     i=i+1
+
+#we use pass for empty if,elif or else block
+# for i in range(1,10):
+#     if i%2==0:
+#         pass
+#     else:
+#         print(i)
+#
+
+#for-else
+#if there is a break in for loop then if the break is never
+#executed it will execute the else statement
+# arr=[10,12,14,12,16]
+# for i in arr:
+#     if i%2!=0:
+#         print(i)
+#         break
+#
+# else:
+#     print("Not found")
+
+from array import *
+# vals=array('u',['a','e','i','o','u'])
+# vals1=array('i',[1,2,3,4,5])
+# new_vals1=array(vals1.typecode,[e**2 for e in vals1])
+# new_vals=array(vals.typecode,[e for e in vals])
+# print(new_vals)
+# print(new_vals1)
+#
+
+
+
+
+
+
+
