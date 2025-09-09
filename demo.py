2465ad4e2cfec7628c0100dd3026c4cad9a4144d
A-N-I-K-A <bushra.anika2018@gmail.com>
2025-09-09 16:18:32 +0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -66,8 +66,101 @@ from array import *
 #
 
 
+#
+# from array import *
+# arr=array('i',[])
+# n=int(input('Enter the length of array:'))
+# for i in range(n):
+#     x=int(input('Enter the value'))
+#     arr.append(x)
 
 
+from numpy import *
+# arr=array([1,2,3,4,5],float)
+#
+# print(arr.dtype)
+# print(arr)
 
 
+# arr=linspace(0,15,16)
+# print(arr)
 
+# arr=arange(1,15,2)
+# print(arr)
+
+# arr=logspace(1,40,5)
+# print(arr)
+
+# arr=zeros(10,int)
+# print(arr)
+
+
+# arr1=array([1,2,3,4,5])
+# arr2=arr1
+# print(arr2)
+# print(arr1)
+# print(id(arr2))
+# print(id(arr1))
+#
+#
+# arr1=array([1,2,3,4,5])
+# arr2=arr1.view()
+# arr1[0]=10
+# print(arr2)
+# print(arr1)
+# print(id(arr2))
+# print(id(arr1))
+#
+# arr1=array([1,2,3,4,5])
+# arr2=arr1.copy()
+# arr1[0]=10
+# print(arr2)
+# print(arr1)
+# print(id(arr2))
+# print(id(arr1))
+#
+
+# arr1=array([
+#             [1,2,3,4,7,6],
+#             [4,5,6,8,9,7]
+# ])
+#
+# print(arr1.ndim)
+# print(arr1.shape)
+# arr2=arr1.flatten()
+# print(arr2)
+# arr3=arr2.reshape(2,6)
+# print(arr3)
+# arr4=arr2.reshape(2,3,2) #gives 2 2d matrix each having 3 1d arrays of 2 items
+# print(arr4)
+
+# arr1=array([[1,2,3],[4,5,6],[7,8,9]])
+# arr2=matrix(arr1)
+# print(arr2)
+# arr3=matrix('1 2 3; 4 5 6; 7 8 9')
+# print(arr3)
+# print(arr3.min())
+# print(arr3.diagonal())
+# print(arr2*arr3)
+
+# def update(x):
+#     print(id(x))#till now the address is same
+#     x=8
+#     print("x",x)
+#     print(id(x))#as we change the value the address will be different
+# x=10
+# print(id(x))
+# update(x)
+# print("x",x)
+#
+#
+#
+# def update(x):
+#     print(id(x))#till now the address is same
+#     x[0]=4
+#     print("x",x)
+#     print(id(x))#the address will be same only
+# x=[1,2,3]
+# print(id(x))
+# update(x)
+# print("x",x)
